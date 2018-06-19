24955ece0c9b18ecabb7881271bad42824c539df
Frediflexta <fredrick.adegoke@gmail.com>
2018-06-19 09:50:05 +0100

    [#158420149] Refactor how the file is started

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import router from './routes/example';
 
 // this creats an instance of the app
 const app = express();
 
 // middleware to parse data i/o as json
+app.use(logger('dev'));
 app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger('dev'));
 
-app.use(router);
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome home, Ride-My_Way');
+});
 
 export default app;
--- server/routes/example.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome home, Ride-My_Way' });
-});
-
-export default router;
