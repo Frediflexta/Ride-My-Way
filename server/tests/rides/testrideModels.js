9b7f9ea7d262161461573195796db2882a2b5208
Frediflexta <fredrick.adegoke@gmail.com>
2018-06-19 12:27:04 +0100

    [#158457537] write tests for the mock models

---
+++ server/tests/rides/testrideModels.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import rideOffers from '../../models/rideOffers';
+
+chai.should();
+
+describe('Ride offers mock-models', () => {
+  it('should be an array', () => {
+    rideOffers.should.be.an('array');
+  })
+
+  it('should contain ride details', () => {
+    const ridesEx_ = [{
+      id: 1,
+      driver: {
+        name: 'Jesse Kalu',
+        car: 'toyota camery',
+        plateNo: 'KTU5768IKJ',
+      },
+      description: {
+        destination: { from: 'ikeja', to: 'Eko-Hotels, Victoria Island' },
+        time: '7:30am',
+      },
+      price: 1500,    
+    }]
+    rideOffers[0].should.deep.equal(ridesEx_[0]);
+  })
+})
