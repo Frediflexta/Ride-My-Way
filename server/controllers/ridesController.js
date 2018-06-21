56178dc129cb4748479fd0d173faf4705bd1aeb6
Frediflexta <fredrick.adegoke@gmail.com>
2018-06-21 01:42:03 +0100

    [#158420965] get all rides

--- server/controllers/ridesController.js
+++ server/controllers/ridesController.js
@@ -8,18 +8,6 @@ class RidesController {
       rides,
     });
   }
-
-  static getRide(req, res) {
-    const rideId = Number(req.params.rideId);
-    rides.find((ride, id) => {
-      if (rideId === (id + 1)) {
-        return res.status(200).send({
-          ride,
-        });
-      }
-      return res.status(404).send({ message: 'Not Found' });
-    });
-  }
 }
 
 export default RidesController;
