396f5fec1470d7f07b3f2e140c19eea21d287d6f
Frediflexta <fredrick.adegoke@gmail.com>
2018-07-04 11:01:32 +0100

    ft/auth/users ensure user tokens are verified on returing or logging into the app. Only authenticated users are allowed to access app functionality [#158816798]

---
+++ server/Middleware/verifyToken.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret = process.env.SECRET;
+
+const AuthVerification = async (req, res, next) => {
+  try {
+    const token = await req.headers['x-access-token'];
+    if (!token) {
+      return res.status(403).json({
+        success: false,
+        message: 'No token provided',
+      });
+    }
+
+    jwt.verify(token, secret, (error, decoded) => {
+      if (error) {
+        return res.status(500).send({
+          success: false,
+          message: 'Failed to authenticate token.',
+        });
+      }
+
+      req.decoded = decoded;
+      next();
+    });
+  } catch (err) {
+    throw err.stack;
+  }
+};
+
+export default AuthVerification;
