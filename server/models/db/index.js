87cf8395ffb0aa289795e5172d675f441f60f8dc
Frediflexta <fredrick.adegoke@gmail.com>
2018-07-05 18:25:17 +0100

    fix bug on table index file

--- server/models/db/index.js
+++ server/models/db/index.js
@@ -2,6 +2,6 @@ import usersTable from '../db/users';
 import ridesTable from '../db/rides';
 import requestsTable from '../db/requests';
 
-setTimeout(usersTable, 500);
-setTimeout(ridesTable, 1000);
+usersTable();
+setTimeout(ridesTable, 500);
 setTimeout(requestsTable, 1000);
